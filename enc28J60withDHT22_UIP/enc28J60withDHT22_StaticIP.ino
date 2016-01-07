352b2e729943acc5fc0667b25325b5480e4e005a
WuHarry <harrywhy32@gmail.com>
2016-01-07 20:47:59 +0800

    version 0.01

---
+++ enc28J60withDHT22_UIP/enc28J60withDHT22_StaticIP.ino
@@ -0,0 +1,88 @@
+#include <SPI.h>
+#include <Dhcp.h>
+#include <ethernet_comp.h>
+#include <UIPClient.h>
+#include <UIPEthernet.h>
+#include <UIPServer.h>
+#include <DHT.h>
+#define DHTPIN A0
+#define DHTTYPE DHT22  
+DHT dht(DHTPIN, DHTTYPE);
+ 
+byte mac[] = {0x74,0x69,0x69,0x2D,0x30,0x31};
+EthernetServer server(80);
+IPAddress ip(192,168,0, 4);
+
+void setup() {
+  Serial.begin(9600);
+  Ethernet.begin(mac);
+  server.begin();
+  dht.begin();
+}
+ 
+void loop() {
+  delay(100);
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) 
+        {
+           // send a standard http response header
+           client.println("HTTP/1.1 200 OK");
+           client.println("Content-Type: text/html");
+           client.println("Connection: close");  // the connection will be closed after completion of the response
+           client.println("Refresh: 1");  // refresh the page automatically every 5 sec
+           client.println();
+           client.println("<!DOCTYPE HTML>");
+           client.println("<html>");
+           // output the value of each analog input pin
+           client.println("<head>");
+           client.println("</head>");
+           client.println("<body>");
+ 
+           float h=dht.readHumidity();
+           float t=dht.readTemperature();  
+           if(isnan(h) || isnan(t))
+           {
+             client.println("Error! No Temperature and Humidity!");
+             client.println("<br />");
+             return;
+           } 
+
+           h=dht.readHumidity();
+           t=dht.readTemperature();
+           client.print("Humidity:");
+           client.print(h);
+           client.print("Temperture:");
+           client.print(t);
+           client.println("<br />");
+           client.println("<br />");
+           client.println("</body>");
+           client.println("</html>");
+           break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+  }
+}
+
---
+++ python client/temp&humi.py
@@ -0,0 +1,105 @@
+# -*- coding: utf8 -*-
+__author__ = 'Harry Wu'
+from Tkinter import *
+import urllib2
+import sys, time
+
+
+class UnicodeStreamFilter:
+    def __init__(self, target):
+        self.target = target
+        self.encoding = 'utf-8'
+        self.errors = 'replace'
+        self.encode_to = self.target.encoding
+    def write(self, s):
+        if type(s) == str:
+            s = s.decode("utf-8")
+        s = s.encode(self.encode_to, self.errors).decode(self.encode_to)
+        self.target.write(s)
+
+if sys.stdout.encoding == 'cp936':
+    sys.stdout = UnicodeStreamFilter(sys.stdout)
+
+
+#从配置文件读取ip列表
+ip_list=[]
+for line in open("ip.config"):
+    line=line.strip('\n')
+    ip_list.append(line)
+
+#ip_1 = raw_input("请输入ip地址: ")
+Temp_1 = input("请输入温度阈值(纯数字可带小数): ")
+Humi_1 = input("请输入湿度阈值(纯数字可带小数): ")
+
+ip_head="http://"
+
+url=[]
+for x in range(0, len(ip_list)):
+    url.append(ip_head+ip_list[x])
+
+#url=ip_head+ip_1
+#初始化列表 ip_response, cont, ip_humi, ip_temp
+initialvalue = ''
+list_length=len(url)
+ip_humi=[initialvalue]*list_length
+ip_temp=[initialvalue]*list_length
+ip_response_temp=urllib2.urlopen(url[0])
+cont_temp=ip_response_temp.read()
+ip_response=[ip_response_temp]*list_length
+cont=[cont_temp]*list_length
+
+while True:
+
+    for x in range(0, list_length):
+        ip_response[x]=urllib2.urlopen(url[x])
+        cont[x]=ip_response[x].read()
+
+    #ip1_response = urllib2.urlopen(url[0])
+    #cont1 = ip1_response.read()
+
+    key1='Humidity:'
+    key2='Temperture:'
+    key3='<br />'
+    # key1='<head>'
+    # key2='</head>'
+    # key3='</body>'
+
+    for x in range(0, list_length):
+        pa=cont[x].find(key1)
+        pt=cont[x].find(key2,pa)
+        #Humidity str
+        ip_humi[x]=cont[x][pa:pt]
+        ip_humi[x]=ip_humi[x][9:]
+        pa=cont[x].find(key2)
+        pt=cont[x].find(key3,pa)
+        #Temperature str
+        ip_temp[x]=cont[x][pa:pt]
+        ip_temp[x]=ip_temp[x][11:]
+    #pa=cont.find(key1)
+    #pt=cont.find(key2,pa)
+    #ip1_humi=cont[pa:pt]
+    #Humidity str
+    #ip1_humi=ip1_humi[9:]
+    #pa=cont.find(key2)
+    #pt=cont.find(key3,pa)
+    #ip_temp=cont[pa:pt]
+    #Temperature str
+    #ip_temp=ip_temp[11:]
+
+    if __name__ == '__main__':
+        for x in range(0, list_length):
+            sys.stdout.write ("第"+str(x+1)+"组 "+"湿度: "+ip_humi[x]+"  温度: "+ip_temp[x]+"\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b")
+
+            if (float(ip_humi[x])>Humi_1):
+                root = Tk()
+                w = Label(root, text=ip_list[x]+"  湿度超标！")
+                w.pack()
+                root.mainloop()
+                time.sleep(10)
+            if (float(ip_temp[x])>Temp_1):
+                root = Tk()
+                w = Label(root, text=ip_list[x]+"  温度超标！")
+                w.pack()
+                root.mainloop()
+                time.sleep(10)
+            time.sleep(2)
